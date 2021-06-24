81d62151df5b5e16f7b51ad42da1238b26a77fac
katinc <katinc2016@gmail.com>
2021-06-24 14:04:06 +0000

    working on like component

---
+++ src/components/common/like.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+//input : liked : boolen
+//output: onClick
+
+class Like extends Component {
+    render() { 
+        return ( <i className="fa fa-address-book" aria-hidden="true"></i>
+        );
+    }
+}
+ 
+export default Like;
